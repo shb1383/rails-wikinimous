f6e5cac1bee6297574a29daba8fdb108786dae73
syrene bartolome <syrene.h.bartolome@icloud.com>
2024-01-06 17:57:41 +0900

    added article model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'destroying old articles...'
+Article.destroy_all
+
+puts 'creating 10 articles...'
+10.times do
+  article = Article.create(
+    title: Faker::Sports::Basketball.team,
+    content: Faker::Sports::Basketball.player
+  )
+  puts "#{article.title} has been created."
+end
+puts 'Done!'
